39617808f0411cb9ac6dde8d179784e3a96da0a8
Anaclaraduartem <anaclaraduartemiranda03@gmail.com>
2022-11-30 11:42:13 -0300

    video 4(fazendo ainda)

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -10,6 +10,30 @@ export class RegistroPage implements OnInit {
 
 formRegistro: FormGroup;
 
+mensagens={
+  nome:[
+    {tipo: 'required', mensagem:'o campo  NOME é obrigatório!'},
+    {tipo:'minlenght', mensagem: 'O Campo NOME precisa ter pelo menos 3 caracteres!'}
+  ],
+  cpf: [
+    { tipo: 'required', mensagem: 'O campo CPF é obrigatório.' },
+  ],
+  email: [
+    { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+    { tipo: 'email', mensagem: 'E-mail Inválido.' },
+  ],
+  senha: [
+    { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+    { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+    { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+  ],
+  confrimasenha: [
+    { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+    { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+    { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+  ],
+  };
+
   constructor(private formBuilder: FormBuilder) {
     this.formRegistro = this.formBuilder.group({
      nome:['',Validators.compose([Validators.required,Validators.minLength(3)])],
