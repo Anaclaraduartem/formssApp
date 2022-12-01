40af806ed49a3428ebae24e2f7bdaeba15eba214
Anaclaraduartem <anaclaraduartemiranda03@gmail.com>
2022-12-01 09:01:29 -0300

    vídeo 4 feito.
    comecei o 5

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,7 @@
+import { StorageService } from './../services/storage.service';
 import { Component, OnInit } from '@angular/core';
-import{FormBuilder,FormGroup,Validators} from '@angular/forms';
+import { Usuario } from '../models/Usuario';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,46 +9,84 @@ import{FormBuilder,FormGroup,Validators} from '@angular/forms';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  formRegistro: FormGroup;
+  usuario: Usuario = new Usuario();
 
-formRegistro: FormGroup;
-
-mensagens={
-  nome:[
-    {tipo: 'required', mensagem:'o campo  NOME é obrigatório!'},
-    {tipo:'minlenght', mensagem: 'O Campo NOME precisa ter pelo menos 3 caracteres!'}
-  ],
-  cpf: [
-    { tipo: 'required', mensagem: 'O campo CPF é obrigatório.' },
-  ],
-  email: [
-    { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
-    { tipo: 'email', mensagem: 'E-mail Inválido.' },
-  ],
-  senha: [
-    { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
-    { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
-    { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
-  ],
-  confrimasenha: [
-    { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
-    { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
-    { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
-  ],
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'o campo  nome é obrigatório!' },
+      {
+        tipo: 'minlenght',
+        mensagem: 'O Campo nome precisa ter pelo menos 3 caracteres!',
+      },
+    ],
+    cpf: [{ tipo: 'required', mensagem: 'O campo CPF é obrigatório.' }],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      {
+        tipo: 'minlength',
+        mensagem: 'A senha deve ter pelo menos 6 caracteres.',
+      },
+      {
+        tipo: 'maxlength',
+        mensagem: 'A senha deve ter no máximo 8 caractéres.',
+      },
+    ],
+    confirmaSenha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      {
+        tipo: 'minlength',
+        mensagem: 'A senha deve ter pelo menos 6 caracteres.',
+      },
+      {
+        tipo: 'maxlength',
+        mensagem: 'A senha deve ter no máximo 8 caractéres.',
+      },
+    ],
   };
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private storageService: StorageService) {
     this.formRegistro = this.formBuilder.group({
-     nome:['',Validators.compose([Validators.required,Validators.minLength(3)])],
-     cpf: ['',Validators.compose([Validators.required])],
-     email:['',Validators.compose([Validators.required,Validators.email])],
-     senha:['',],
-     confirmaSenha:['',Validators.compose([Validators.required,Validators.minLength(6)])]
+      nome: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      cpf: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(8),
+        ]),
+      ],
+      confirmaSenha: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(8),
+        ]),
+      ],
     });
-   }
-
-  ngOnInit() {
   }
-salvarRegistro(){
-  console.log('Formulário: ',this.formRegistro.valid);
-}
+
+  ngOnInit() {}
+
+  async salvarRegistro() {
+   if(this.formRegistro.valid){
+    this.usuario.nome = this.formRegistro.value.nome;
+    this.usuario.cpf = this.formRegistro.value.cpf;
+    this.usuario.email = this.formRegistro.value.email;
+    this.usuario.senha = this.formRegistro.value.senha;
+    await this.storageService.set(this.usuario.email, this.usuario);
+   } else{
+     alert('Formulário Inválido!');
+   }
+  }
 }
